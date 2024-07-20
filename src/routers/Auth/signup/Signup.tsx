4294347683a2c197f06f076ac47faf804fb5159a
chaeyoung103 <thdcodud103@naver.com>
2024-07-21 02:29:49 +0900

    feat: 회원가입 api 연동

--- src/routers/Auth/signup/Signup.tsx
+++ src/routers/Auth/signup/Signup.tsx
@@ -3,7 +3,7 @@ import Txt from '@components/common/text/Txt';
 import PrimaryButton from '@components/common/button/PrimaryButton';
 import TextInput from '@components/common/input/TextInput';
 import { CONFIG, INPUT_TYPE, InputType } from '@constants/form';
-import { FieldErrors, FormProvider, useForm, useFormContext } from 'react-hook-form';
+import { FormProvider, useForm } from 'react-hook-form';
 import { Col } from '@components/common/flex/Flex';
 import { LogoIcon } from '@icons/index';
 import Layout from '@components/common/layout/Layout';
@@ -11,23 +11,29 @@ import { colors } from '@styles/theme';
 import ErrorMessage from '@components/auth/ErrorMessage';
 import { generateNickname } from '@utils/generateNickname';
 import { useEffect, useState } from 'react';
-
-interface SignUpRequestDTO {
-  email: string;
-  password: string;
-  confirmPassword: string;
-  nickName: string;
-}
+import { useMutation } from 'react-query';
+import { signup } from '@apis/auth/auth';
 
 const Signup = () => {
   const methods = useForm<SignUpRequestDTO>({ mode: 'onChange' });
   const [nickname, setNickname] = useState('');
   const {
-    register,
-    handleSubmit,
     watch,
     formState: { errors },
   } = methods;
+
+  // useMutation 훅을 사용하여 signup 함수를 호출합니다.
+  const { mutate, isLoading, isError, data, error } = useMutation(
+    (signUpRequest: SignUpRequestDTO) => signup(signUpRequest),
+    {
+      onSuccess: (data) => {
+        console.log('회원가입 성공:', data);
+      },
+      onError: (error) => {
+        console.error('회원가입 실패:', error);
+      },
+    }
+  );
 
   const isConfirmPasswordError =
     !watch(INPUT_TYPE.CONFIRMPASSWORD) ||
@@ -36,6 +42,17 @@ const Signup = () => {
   const handleCheckError = (id: InputType, regex: RegExp) => {
     const value = watch(id);
     return !regex.test(value);
+  };
+
+  const handleSignup = () => {
+    const signUpRequest: SignUpRequestDTO = {
+      nickName: watch(INPUT_TYPE.NICKNAME),
+      email: watch(INPUT_TYPE.EMAIL),
+      password: watch(INPUT_TYPE.PASSWORD),
+      confirmPassword: watch(INPUT_TYPE.CONFIRMPASSWORD),
+    };
+
+    mutate(signUpRequest);
   };
 
   const renderError = (id: keyof typeof INPUT_TYPE): React.ReactNode => {
@@ -57,14 +74,11 @@ const Signup = () => {
         />
       );
     });
-
-    // null 값을 제거하고, 에러 메시지 컴포넌트 배열을 반환합니다.
     return errorComponents?.filter(Boolean) || null;
   };
 
   useEffect(() => {
-    const generatedNickname = generateNickname();
-    setNickname(generatedNickname);
+    setNickname(generateNickname());
   }, []);
 
   return (
@@ -150,7 +164,7 @@ const Signup = () => {
             <PrimaryButton
               title="가입"
               disabled={!methods.formState.isValid || isConfirmPasswordError}
-              onClick={() => console.log(methods.formState)}
+              onClick={handleSignup}
             />
           </Col>
         </Col>
