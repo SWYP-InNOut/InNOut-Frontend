00904280bc535189110c51e21f29ec69d458a8e3
chaeyoung103 <thdcodud103@naver.com>
2024-07-22 03:48:04 +0900

    feat: 마이홈 ui 생성

---
+++ src/routers/home/MyHome.styles.tsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+import { colors } from '@styles/theme';
+
+export const ChattingBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 260px;
+  border-radius: 8px;
+  border: 2px solid ${colors.gray200};
+`;
---
+++ src/routers/home/MyHome.tsx
@@ -0,0 +1,83 @@
+import { Col, Row } from '@components/common/flex/Flex';
+import Layout from '@components/common/layout/Layout';
+import Txt from '@components/common/text/Txt';
+import { css } from '@emotion/react';
+import { LogoIcon, MenuIcon } from '@icons/index';
+import { colors } from '@styles/theme';
+import { ChattingBox } from './MyHome.styles';
+import HomeMenuSlider from './HomeMenuSlider';
+import { useState } from 'react';
+
+const MyHome = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div
+      css={css`
+        width: inherit;
+        height: auto;
+        min-height: 100vh;
+        position: relative;
+        overflow-x: hidden;
+      `}
+    >
+      <Layout
+        hasHeader={true}
+        HeaderLeft={
+          <button
+            css={css`
+              background-color: transparent;
+              border: none;
+              cursor: pointer;
+            `}
+            onClick={toggleMenu}
+          >
+            <MenuIcon />
+          </button>
+        }
+        HeaderCenter={
+          <div
+            css={css`
+              width: 104px;
+            `}
+          >
+            <LogoIcon />
+          </div>
+        }
+        HeaderRight={
+          <button
+            css={css`
+              width: 29px;
+              cursor: pointer;
+            `}
+          >
+            <Txt variant="b16" color={colors.yellow700}>
+              공유
+            </Txt>
+          </button>
+        }
+      >
+        <Col padding={'24px 28px'}>
+          <Row gap={'4'} justifyContent="start" alignItems="end">
+            <Txt variant="h28" lineHeight={42}>
+              사용자닉네임최대길이
+            </Txt>
+            <Txt variant="t18" color={colors.lightGray}>
+              님의 홈
+            </Txt>
+          </Row>
+        </Col>
+        <Col padding={'0 16px'}>
+          <ChattingBox></ChattingBox>
+        </Col>
+      </Layout>
+      <HomeMenuSlider isOpen={isOpen} handleMenu={toggleMenu} />
+    </div>
+  );
+};
+
+export default MyHome;
