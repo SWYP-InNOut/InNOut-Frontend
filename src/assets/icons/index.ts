57c2f4978c4f8a5360d6f96683b26773c9f24237
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-07-22 00:04:53 +0900

    chore: 완료 화면 svg 추가

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -5,5 +5,17 @@ import Logo from '@icons/logo_32.svg?react'; // 글씨 없는 로고
 import GoogleIcon from '@icons/google.svg?react';
 import KakaoIcon from '@icons/kakao.svg?react';
 import RightArrowIcon from '@icons/rightarrow.svg?react';
+import SendEmailIcon from '@icons/sendEmail.svg?react';
+import SendPwdIcon from '@icons/sendPwd.svg?react';
 
-export { LogoIcon, ErrorIcon, CheckIcon, Logo, KakaoIcon, GoogleIcon, RightArrowIcon };
+export {
+  LogoIcon,
+  ErrorIcon,
+  CheckIcon,
+  Logo,
+  KakaoIcon,
+  GoogleIcon,
+  RightArrowIcon,
+  SendEmailIcon,
+  SendPwdIcon,
+};
