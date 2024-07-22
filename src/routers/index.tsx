40e656741df78e9bd26c5ef8199cbe8c551ac7f0
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-07-22 21:04:37 +0900

    chore: 라우팅 설정

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -9,17 +9,20 @@ import Login from '@routers/login/Login';
 import PwdSearch from '@routers/pwdSearch/PwdSearch';
 import NickNameSetting from './nickname/NickNameSetting';
 import MyHome from '@routers/home/MyHome';
-
+import NickNameChange from './home/NickNameChange';
+import PwdChange from './home/PwdChange';
 
 const Router = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <>
         <Route index element={<MyHome />} />
+        <Route path="nickname" element={<NickNameChange />} />
         <Route path="signup" element={<Signup />} />
         <Route path="login" element={<Login />} />
         <Route path="pwdsearch" element={<PwdSearch />} />
         <Route path="setting" element={<NickNameSetting />} />
+        <Route path="pwd" element={<PwdChange />} />
       </>
     )
   );
