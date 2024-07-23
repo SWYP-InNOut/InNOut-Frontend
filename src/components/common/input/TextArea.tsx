00a8b55b1b36548bf672439f912252d69d6d1a7b
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-07-24 02:39:53 +0900

    fix: 폰트 스타일 수정

--- src/components/common/input/TextArea.tsx
+++ src/components/common/input/TextArea.tsx
@@ -1,12 +1,12 @@
-import { colors } from '@styles/theme';
-import { InputType } from '@constants/form';
+import { colors, fontStyles } from '@styles/theme';
+import { PostInputType } from '@constants/postFormConfig';
 import { RegisterOptions, useFormContext } from 'react-hook-form';
 import styled from '@emotion/styled';
 import { useEffect, useState } from 'react';
 
 interface TextAreaProps {
   type?: string;
-  id: InputType;
+  id: PostInputType;
   options?: RegisterOptions;
   placeholder?: string;
   content?: string;
@@ -43,15 +43,15 @@ const TextArea = (props: TextAreaProps) => {
 const StyledInput = styled.textarea`
   width: 100%;
   padding: 16px 16px;
+  max-height: 224px;
   color: ${colors.darkGray};
-  font-size: 1.6rem;
-  font-weight: 400;
-  line-height: 24px;
-  font-family: 'LINE Seed Sans', sans-serif;
   background-color: transparent;
   border: none;
   outline: none;
-
+  font-size: ${fontStyles.b16.fontSize};
+  font-weight: ${fontStyles.b16.fontWeight};
+  line-height: ${fontStyles.b16.lineHeight};
+  font-family: ${fontStyles.b16.fontFamily};
   &::placeholder {
     color: ${colors.lightGray};
   }
@@ -59,7 +59,6 @@ const StyledInput = styled.textarea`
 
 const InputContainer = styled.div<{ isFocused: boolean }>`
   width: 100%;
-  height: 224px;
   border-radius: 8px;
   background-color: ${({ isFocused }) => (isFocused ? colors.yellow300 : colors.gray100)};
   transition: background-color 0.2s ease;
