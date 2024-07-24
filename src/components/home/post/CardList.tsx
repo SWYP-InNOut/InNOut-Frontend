a4121b05451b16b0fb95bb4d0cf8677035e1fc08
chaeyoung103 <thdcodud103@naver.com>
2024-07-25 02:00:26 +0900

    feat: myhome ui 퍼블리싱

--- src/components/home/post/CardList.tsx
+++ src/components/home/post/CardList.tsx
@@ -1,12 +1,11 @@
-import styled from '@emotion/styled/macro';
+import styled from '@emotion/styled';
 
 const CardList = ({ imgList }: { imgList: string[] }) => {
   return (
     <CardContainer>
-      <Card src="https://via.placeholder.com/150" />
-      <Card src="https://via.placeholder.com/150" />
-      <Card src="https://via.placeholder.com/150" />
-      <Card src="https://via.placeholder.com/150" />
+      {imgList.map((img, index) => (
+        <Card key={index} src={img} />
+      ))}
     </CardContainer>
   );
 };
@@ -15,15 +14,15 @@ export default CardList;
 
 const CardContainer = styled.div`
   width: 100%;
-  padding: 20px 16px;
   gap: 8px;
   display: grid;
   grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: auto;
 `;
 
 const Card = styled.img`
   width: 100%;
   aspect-ratio: 1 / 1;
   background-color: lightgray;
-  margin-bottom: 10px;
+  border-radius: 2px;
 `;
