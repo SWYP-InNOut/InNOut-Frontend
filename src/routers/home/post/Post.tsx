41f78dbb61867106d80970bc0b1db804385caead
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-07-24 13:57:21 +0900

    feat: 사진등록 안할시 경고 알람

--- src/routers/home/post/Post.tsx
+++ src/routers/home/post/Post.tsx
@@ -8,74 +8,129 @@ import { Col } from '@components/common/flex/Flex';
 import TextArea from '@components/common/input/TextArea';
 import PrimaryButton from '@components/common/button/PrimaryButton';
 import Picture from '@components/home/post/Picture';
-import { PostInputType } from '@constants/postFormConfig';
 import { css } from '@emotion/react';
 import AlertModal from '@components/common/alert/AlertModal';
 import { colors } from '@styles/theme';
-import { Navigate, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Post = () => {
   const navigate = useNavigate();
   const [isCloseAlert, setIsCloseAlert] = useState(false);
+  const [alertContent, setAlertContent] = useState<React.ReactNode>(null);
+  const [alertBtn, setAlertBtn] = useState<React.ReactNode>(null);
   const methods = useForm({
     mode: 'onChange',
     defaultValues: {
+      title: '',
       pictures: [],
       mainPicture: null,
+      inContent: '',
+      outContent: '',
     },
   });
-  const { handleSubmit } = methods;
+  const { handleSubmit, getValues } = methods;
 
-  const onSubmit = (data: any) => {
+  const onFormSubmit = (data: any) => {
+    console.log('fff');
     console.log(data);
+    if (handleValidation()) {
+      console.log(data);
+    }
   };
 
-  const handleCloseBtn = () => {
-    console.log('close');
-    setIsCloseAlert(true);
-  };
   const handleExitBtn = () => {
     navigate(-1);
   };
+
+  //상단 X 표시 눌렀을 경우
+  const handleCloseBtn = () => {
+    setIsCloseAlert(true);
+    setAlertBtn(
+      <Col gap={'8'}>
+        <PrimaryButton
+          title="계속 작성"
+          onClick={() => {
+            setIsCloseAlert(false);
+          }}
+          color={colors.red600}
+        />
+        <PrimaryButton
+          title="나가기"
+          onClick={handleExitBtn}
+          color={colors.gray100}
+          fontColor={colors.lightGray}
+        />
+      </Col>
+    );
+    setAlertContent(
+      <Col gap={'12'} alignItems="center" justifyContent="center">
+        <Txt variant="t20">정말 나가시겠어요?</Txt>
+        <Txt variant="b16" align="center">
+          지금 페이지를 나가면
+          <br />
+          입력한 내용이 모두 없어져요
+        </Txt>
+      </Col>
+    );
+  };
+  const handleValidation = () => {
+    const { title, pictures } = getValues();
+    console.log('ffff');
+    console.log(title);
+
+    //  이거 작동이 안됨 수정 필요
+    if (title === '') {
+      setAlertContent(
+        <Txt variant="b16" align="center">
+          제목은 필수
+        </Txt>
+      );
+      setAlertBtn(
+        <PrimaryButton
+          title="확인"
+          onClick={() => {
+            setIsCloseAlert(false);
+          }}
+          color={colors.red600}
+        />
+      );
+      setIsCloseAlert(true);
+      return false;
+    }
+    if (pictures.length < 1) {
+      setAlertContent(
+        <Col gap={'12'} alignItems="center">
+          <Txt variant="t20">사진은 중요!</Txt>
+          <Txt variant="b16" align="center">
+            최소 1장은 필수예요
+          </Txt>
+        </Col>
+      );
+      setAlertBtn(
+        <PrimaryButton
+          title="확인"
+          onClick={() => {
+            setIsCloseAlert(false);
+          }}
+          color={colors.red600}
+        />
+      );
+      setIsCloseAlert(true);
+      return false;
+    }
+    return true;
+  };
+
   return (
     <>
-      <AlertModal
-        isOpen={isCloseAlert}
-        content={
-          <Col gap={'12'} alignItems="center" justifyContent="center">
-            <Txt variant="t20">정말 나가시겠어요?</Txt>
-            <Txt variant="b16" align="center">
-              지금 페이지를 나가면
-              <br />
-              입력한 내용이 모두 없어져요
-            </Txt>
-          </Col>
-        }
-        button={
-          <Col gap={'8'}>
-            <PrimaryButton
-              title="계속 작성"
-              onClick={() => {
-                setIsCloseAlert(false);
-              }}
-              color={colors.red600}
-            />
-            <PrimaryButton
-              title="나가기"
-              onClick={handleExitBtn}
-              color={colors.gray100}
-              fontColor={colors.lightGray}
-            />
-          </Col>
-        }
-      />
+      <AlertModal isOpen={isCloseAlert} content={alertContent} button={alertBtn} />
       <Layout
         hasHeader={true}
         HeaderCenter={<Txt variant="t20">새 게시물</Txt>}
         HeaderRight={<CloseIcon onClick={handleCloseBtn} />}
       >
         <FormProvider {...methods}>
-          <form onSubmit={handleSubmit(onSubmit)}>
+          <form onSubmit={handleSubmit(onFormSubmit)}>
             <Col padding={'0 16px 48px 16px'} gap={'32'} margin={'32px 0 0 0'}>
               <Col gap={'8'}>
                 <Txt variant="t20">제목</Txt>
@@ -93,7 +148,6 @@ const Post = () => {
                 <TextArea
                   id="inContent"
                   placeholder={`버릴 수 없는 이유를 알려주세요.\n자세히 들려줄수록 투표수가 올라가요.`}
-                  options={{ required: '내용은 필수입니다.' }}
                 />
               </Col>
               <Col gap={'8'}>
@@ -101,10 +155,9 @@ const Post = () => {
                 <TextArea
                   id="outContent"
                   placeholder={`버리고 싶은 이유를 알려주세요.\n자세히 들려줄수록 투표수가 올라가요.`}
-                  options={{ required: '내용은 필수입니다.' }}
                 />
               </Col>
-              <PrimaryButton title="등록" onClick={handleSubmit(onSubmit)} type="submit" />
+              <PrimaryButton title="등록" type="submit" />
             </Col>
           </form>
         </FormProvider>
