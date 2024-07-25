336ac0246959118533c580ae47133b476e4dd306
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-07-26 00:51:05 +0900

    feat: 공유 클릭시 AlertModal

--- src/routers/home/MyHome.tsx
+++ src/routers/home/MyHome.tsx
@@ -11,12 +11,15 @@ import CardList from '@components/home/post/CardList';
 import Filter from '@components/common/filter/Filter';
 import styled from '@emotion/styled';
 import PreviewChat from '@components/chat/PreviewChat';
+import AlertModal from '@components/common/alert/AlertModal';
+import PrimaryButton from '@components/common/button/PrimaryButton';
 
 const MyHome = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [selectList, setSelectList] = useState(['최신순', 'In 많은순', 'Out 많은순', '오래된순']);
   const [isFilterOpen, setIsFilterOpen] = useState(false);
   const [selectedFilter, setSelectedFilter] = useState('최신순');
+  const [isShareOpenModal, setIsShareModal] = useState(false);
 
   const toggleFilter = () => {
     setIsFilterOpen(!isFilterOpen);
@@ -26,102 +29,137 @@ const MyHome = () => {
     setIsOpen(!isOpen);
   };
 
+  const handleShareBtn = () => {
+    setIsShareModal(!isShareOpenModal);
+  };
+
   return (
-    <Layout
-      overflow={isOpen ? 'hidden' : 'auto'}
-      hasHeader={true}
-      HeaderLeft={
-        <button
+    <>
+      <AlertModal
+        isShare={true}
+        isOpen={isShareOpenModal}
+        close={handleShareBtn}
+        content={
+          <Col gap={'12'} alignItems="center">
+            <Txt variant="t20">친구를 초대해요</Txt>
+
+            <Col alignItems="center">
+              <Txt variant="b16">이 링크로 초대받은 친구는</Txt>
+              <div>
+                <Txt variant="t16">로그인 없이 홈에 놀러 와 투표할</Txt>
+                <Txt variant="b16">수 있으니</Txt>
+              </div>
+              <Txt variant="b16">친구들에게 공유해 보세요!</Txt>
+              <br />
+              <div>
+                <Txt variant="b16">
+                  링크는
+                  <Txt variant="t16"> 72시간 유효</Txt>
+                  <Txt variant="b16">해요</Txt>
+                </Txt>
+              </div>
+            </Col>
+          </Col>
+        }
+        button={<PrimaryButton title="링크 복사" color={colors.red600} fontColor={colors.white} />}
+      />
+      <Layout
+        overflow={isOpen ? 'hidden' : 'auto'}
+        hasHeader={true}
+        HeaderLeft={
+          <button
+            css={css`
+              background-color: transparent;
+              border: none;
+              cursor: pointer;
+            `}
+            onClick={toggleMenu}
+          >
+            <MenuIcon />
+          </button>
+        }
+        HeaderCenter={
+          <div
+            css={css`
+              width: 104px;
+            `}
+          >
+            <LogoIcon />
+          </div>
+        }
+        HeaderRight={
+          <button
+            css={css`
+              width: 29px;
+              cursor: pointer;
+            `}
+            onClick={handleShareBtn}
+          >
+            <Txt variant="b16" color={colors.yellow700}>
+              공유
+            </Txt>
+          </button>
+        }
+      >
+        <Col padding={'24px 28px'}>
+          <Row gap={'4'} justifyContent="start" alignItems="end">
+            <Txt variant="h28" lineHeight={42}>
+              사용자닉네임최대길이
+            </Txt>
+            <Txt variant="t18" color={colors.lightGray}>
+              님의 홈
+            </Txt>
+          </Row>
+        </Col>
+        <Col padding={'0 16px'}>
+          <PreviewChat />
+        </Col>
+
+        <Row
+          padding={'0 16px'}
+          justifyContent={'flex-end'}
           css={css`
-            background-color: transparent;
-            border: none;
-            cursor: pointer;
+            width: 100%;
           `}
-          onClick={toggleMenu}
         >
-          <MenuIcon />
-        </button>
-      }
-      HeaderCenter={
+          <Filter
+            selectList={selectList}
+            isOpen={isFilterOpen}
+            handleIsOpen={toggleFilter}
+            selected={selectedFilter}
+            handleSelect={setSelectedFilter}
+            onClick={toggleFilter}
+          />
+        </Row>
+
         <div
           css={css`
-            width: 104px;
+            padding: 0 16px 20px;
           `}
         >
-          <LogoIcon />
+          <CardList
+            imgList={['https://via.placeholder.com/150', 'https://via.placeholder.com/150']}
+          />
         </div>
-      }
-      HeaderRight={
-        <button
+        <Col
+          padding={'29.5px 0'}
           css={css`
-            width: 29px;
-            cursor: pointer;
+            background-color: ${colors.yellow300};
+            text-align: center;
           `}
         >
-          <Txt variant="b16" color={colors.yellow700}>
-            공유
+          <Txt variant="b16" color={colors.lightGray}>
+            stuffinout@gmail.com
           </Txt>
-        </button>
-      }
-    >
-      <Col padding={'24px 28px'}>
-        <Row gap={'4'} justifyContent="start" alignItems="end">
-          <Txt variant="h28" lineHeight={42}>
-            사용자닉네임최대길이
+          <Txt variant="c11" color={colors.lightGray}>
+            Copyright ©Teamname. All rights reserved.
           </Txt>
-          <Txt variant="t18" color={colors.lightGray}>
-            님의 홈
-          </Txt>
-        </Row>
-      </Col>
-      <Col padding={'0 16px'}>
-        <PreviewChat />
-      </Col>
-
-      <Row
-        padding={'0 16px'}
-        justifyContent={'flex-end'}
-        css={css`
-          width: 100%;
-        `}
-      >
-        <Filter
-          selectList={selectList}
-          isOpen={isFilterOpen}
-          handleIsOpen={toggleFilter}
-          selected={selectedFilter}
-          handleSelect={setSelectedFilter}
-          onClick={toggleFilter}
-        />
-      </Row>
-
-      <div
-        css={css`
-          padding: 0 16px 20px;
-        `}
-      >
-        <CardList
-          imgList={['https://via.placeholder.com/150', 'https://via.placeholder.com/150']}
-        />
-      </div>
-      <Col
-        padding={'29.5px 0'}
-        css={css`
-          background-color: ${colors.yellow300};
-          text-align: center;
-        `}
-      >
-        <Txt variant="b16" color={colors.lightGray}>
-          stuffinout@gmail.com
-        </Txt>
-        <Txt variant="c11" color={colors.lightGray}>
-          Copyright ©Teamname. All rights reserved.
-        </Txt>
-      </Col>
-      <HomeMenuSlider isOpen={isOpen} handleMenu={toggleMenu} />
-      <StyledPencilIcon />
-      <AddButton />
-    </Layout>
+        </Col>
+        <HomeMenuSlider isOpen={isOpen} handleMenu={toggleMenu} />
+        <StyledPencilIcon />
+        <AddButton />
+      </Layout>
+    </>
   );
 };
 
