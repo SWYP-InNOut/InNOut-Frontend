a2dcdb9c70262a23043624a033e7e28e4627469a
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-07-25 23:51:11 +0900

    feat: In&Out 버튼 컴포넌트

--- src/routers/home/detail/Detail.tsx
+++ src/routers/home/detail/Detail.tsx
@@ -24,38 +24,15 @@ import { colors } from '@styles/theme';
 import ScrollProgress from '@components/home/post/ScrollProgress';
 import TextArea from '@components/common/input/TextArea';
 import ContentContainer from '@components/home/post/ContentContainer';
+import ImagePicker from '@components/home/post/Image/ImagePicker';
+import InOutVoting from '@components/home/\binout/InOutVoting';
 
 const Detail = () => {
   const inContent: string =
     '사용자가 작성한 내용부분은 inout을 작성할 때, 사용자가 작성한 내용 중 긴 내용에 해당하는 텍스트 박스 사이즈로 통일됩니다. ';
   const outContent: string =
     '사용자가 작성한 내용부분은 inout을 작성할 때, 사용자가 작성한 내용 중 긴 내용에 해당하는 텍스트 박스 사이즈로 통일됩니다. 사용자가 작성한 내용부분은 inout을 작성할 때, 사용자가 작성한 내용 중 긴 내용에 해당하는 텍스트 박스 사이즈로 통일됩니다. ';
-  const calculateHeight = (content: string): number => {
-    const averageCharHeight = 1.2; // 각 문자의 평균 높이를 1.2px로 가정합니다. 필요에 따라 조정 가능합니다.
-    return content.length * averageCharHeight;
-  };
-  const handleResize = (
-    setHeightSize: Dispatch<SetStateAction<number>>,
-    inContent: string,
-    outContent: string
-  ) => {
-    const maxContentLength = Math.max(inContent.length, outContent.length);
-    const newHeight = calculateHeight(maxContentLength.toString());
-    setHeightSize(newHeight);
-  };
-
   const [heightSize, setHeightSize] = useState<number>(0);
-
-  useEffect(() => {
-    const resizeHandler = () => handleResize(setHeightSize, inContent, outContent);
-
-    resizeHandler();
-    window.addEventListener('resize', resizeHandler);
-    console.log(heightSize);
-    return () => {
-      window.removeEventListener('resize', resizeHandler);
-    };
-  }, [inContent, outContent]);
 
   return (
     <Layout HeaderLeft={<LeftArrowIcon />} HeaderRight={<Txt variant="b16">수정</Txt>}>
@@ -64,13 +41,7 @@ const Detail = () => {
           margin-top: 12px;
         `}
       >
-        <TempImg
-          css={css`
-            width: 100%;
-            height: 100%;
-            aspect-ratio: 1 / 1;
-          `}
-        />
+        <ImagePicker />
       </div>
 
       <Col padding={'32px 16px'} gap={'4'}>
@@ -85,32 +56,7 @@ const Detail = () => {
       <Contour />
       <Col padding={'32px 16px'} gap={'32'} margin={'0 0 12px 0'}>
         <SquareLogoIcon />
-        <Row gap={'8'}>
-          <Col
-            padding={'16px 8px'}
-            gap={'6'}
-            css={css`
-              background-color: ${colors.yellow200};
-              border-radius: 8px;
-              cursor: pointer;
-            `}
-          >
-            <LottieContainer path={'/src/assets/lottie/Out.json'} />
-            <Logo32>In</Logo32>
-          </Col>
-          <Col
-            padding={'16px 8px'}
-            gap={'6'}
-            css={css`
-              background-color: ${colors.yellow200};
-              border-radius: 8px;
-              cursor: pointer;
-            `}
-          >
-            <LottieContainer path={'/src/assets/lottie/Out.json'} />
-            <Logo32>Out</Logo32>
-          </Col>
-        </Row>
+        <InOutVoting />
       </Col>
       <Contour />
       <Col padding={'32px 16px'} gap={'32'}>
@@ -120,11 +66,11 @@ const Detail = () => {
         </Row>
         <Col gap={'8'}>
           <Txt variant="t20">In! 하고 싶은 이유</Txt>
-          <ContentContainer content={inContent} height={heightSize} />
+          <ContentContainer content={inContent} />
         </Col>
         <Col gap={'8'}>
           <Txt variant="t20">Out! 하고 싶은 이유</Txt>
-          <ContentContainer content={outContent} height={heightSize} />
+          <ContentContainer content={outContent} />
         </Col>
       </Col>
       <Contour />
