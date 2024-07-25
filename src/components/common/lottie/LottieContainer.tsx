11e088e387da67c540201f7649974e05f8d33e3f
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-07-25 23:50:18 +0900

    fix: lottie animation 추가

--- src/components/common/lottie/LottieContainer.tsx
+++ src/components/common/lottie/LottieContainer.tsx
@@ -1,34 +1,50 @@
 import React, { useEffect, useRef } from 'react';
 import styled from '@emotion/styled';
-import Lottie from 'lottie-web';
+import Lottie, { AnimationItem } from 'lottie-web';
 
 export type LottieContainerProps = {
   path: string;
+  isPlay: boolean;
 };
 
-const Root = styled.div`
-  width: 100%;
-  height: 100%;
-`;
-
-export const LottieContainer: React.FC<LottieContainerProps> = ({ path }) => {
+export const LottieContainer: React.FC<LottieContainerProps> = ({ path, isPlay }) => {
   const lottieContainer = useRef<HTMLDivElement>(null);
+  const animRef = useRef<AnimationItem | null>(null);
 
   useEffect(() => {
     if (lottieContainer.current) {
-      const anim = Lottie.loadAnimation({
+      animRef.current = Lottie.loadAnimation({
         container: lottieContainer.current,
         renderer: 'svg',
         loop: true,
-        autoplay: true,
+        autoplay: isPlay,
         path: path,
       });
 
-      return () => anim.destroy();
+      return () => {
+        if (animRef.current) {
+          animRef.current.destroy();
+        }
+      };
     }
   }, [path]);
+
+  useEffect(() => {
+    if (animRef.current) {
+      if (isPlay) {
+        animRef.current.play();
+      } else {
+        animRef.current.stop();
+      }
+    }
+  }, [isPlay]);
 
   return <Root ref={lottieContainer} />;
 };
 
 export default LottieContainer;
+
+const Root = styled.div`
+  width: 100%;
+  height: 100%;
+`;
