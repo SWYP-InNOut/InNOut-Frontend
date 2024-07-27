aca0f1db42fdf3616597e32955d39f962f0123b4
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-07-27 16:50:50 +0900

    feat: others api

---
+++ src/apis/others.ts
@@ -0,0 +1,41 @@
+import { BaseResponse } from '@interfaces/api/base';
+import { apiClient } from '@stores/auth';
+
+//타사용자룸 전체보기(방문자순)
+export const getOthers = async (): Promise<BaseResponse<string>> => {
+  try {
+    const response = await apiClient.get('/others');
+    return response.data;
+  } catch (error) {
+    console.error('타사용자룸 전체보기(방문자순) 실패:', error);
+    throw error;
+  }
+};
+
+//특정 사용자 룸 / 물건 리스트
+export const postOthersRoom = async (request: RoomRequestDTO): Promise<BaseResponse<string>> => {
+  try {
+    const response = await apiClient.post('/others/room', { request });
+    return response.data;
+  } catch (error) {
+    console.error('특정 사용자 룸 / 물건 리스트 실패:', error);
+    throw error;
+  }
+};
+
+// /others/post/{postId}
+//특정 사용자 게시물 상세보기
+export const postOthersRoompost = async (
+  memberId: string,
+  postId: number
+): Promise<BaseResponse<string>> => {
+  try {
+    const response = await apiClient.get(`/others/post/${postId}`, {
+      params: { memberId: memberId },
+    });
+    return response.data;
+  } catch (error) {
+    console.error('특정 사용자 게시물 상세보기 실패:', error);
+    throw error;
+  }
+};
