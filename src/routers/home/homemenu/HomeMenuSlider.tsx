81b7a45671bea42a73a10f061b420e92f68fb0c3
chaeyoung103 <thdcodud103@naver.com>
2024-07-30 03:19:22 +0900

    fix: 닉네임 가운데 정렬로 수정

--- src/routers/home/homemenu/HomeMenuSlider.tsx
+++ src/routers/home/homemenu/HomeMenuSlider.tsx
@@ -24,23 +24,21 @@ import useAuthStore from '@stores/auth';
 import { getIsPublic } from '@apis/myroom';
 import { useMutation } from 'react-query';
 import { AxiosError } from 'axios';
-import { set } from 'react-hook-form';
 import { IsPublicResponseDTO } from '@interfaces/api/room';
 
 const HomeMenuSlider = ({
   isOpen,
   handleMenu,
   isPublic,
-  memberName,
 }: {
   isOpen: boolean;
   isPublic: boolean;
-  memberName: string;
   handleMenu: () => void;
 }) => {
   const [isOn, setIsOn] = useState(false);
   const [isLogoutAlert, setIsLogoutAlert] = useState(false);
   const isLogin = useAuthStore((store) => store.isLoggedIn);
+  const nickName = useAuthStore((store) => store.nickname);
 
   const getIsPublicMutation = useMutation(getIsPublic, {
     onSuccess: (data) => {
@@ -64,8 +62,6 @@ const HomeMenuSlider = ({
 
   const handlePublic = () => {
     getIsPublicMutation.mutate();
-    console.log();
-    console.log('나의 홈 공개');
   };
 
   const handleIntroduce = () => {
@@ -79,9 +75,9 @@ const HomeMenuSlider = ({
   const logout = useAuthStore((state) => state.logout);
   const renderTitle = () => {
     return isLogin ? (
-      <Row padding={'0 28px 64px'} gap={'4'} justifyContent="start" alignItems="end">
+      <Row padding={'0 28px 64px'} gap={'4'} justifyContent="center" alignItems="end">
         <Txt variant="h28" lineHeight={36}>
-          {memberName}
+          {nickName}
         </Txt>
         <Txt variant="t18" color={colors.lightGray}>
           님의 홈
@@ -128,11 +124,10 @@ const HomeMenuSlider = ({
               onClick={async () => {
                 try {
                   await logout();
-                  console.log('로그아웃 성공');
                   setIsLogoutAlert(false);
                   navigate('/login');
-                } catch (error) {
-                  console.error('로그아웃 실패', error);
+                } catch (e) {
+                  console.log('로그아웃 실패:', e);
                 }
               }}
               color={colors.red600}
