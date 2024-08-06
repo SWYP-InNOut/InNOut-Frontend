59c681d8d9fb0660b64ba9df1f4ea3f48364eb97
chaeyoung103 <thdcodud103@naver.com>
2024-08-07 02:29:44 +0900

    feat: 디테일한 디자인 기능 구현
    press 효과, hover 효과 등

--- src/components/home/post/CardList.tsx
+++ src/components/home/post/CardList.tsx
@@ -1,7 +1,8 @@
-import { Col } from '@components/common/flex/Flex';
+import { Col, Row } from '@components/common/flex/Flex';
 import Txt from '@components/common/text/Txt';
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
-import { Charactor } from '@icons/index';
+import { Charactor, EyesIcon } from '@icons/index';
 import { Post } from '@interfaces/api/room';
 import { colors } from '@styles/theme';
 import { useNavigate } from 'react-router-dom';
@@ -25,11 +26,37 @@ const CardList = ({ postList }: { postList?: Post[] }) => {
       ) : (
         <CardContainer>
           {postList.map((post, index) => (
-            <Card
-              key={index}
-              src={post.imgUrl}
-              onClick={() => navigate(`/detail/${post.postId}`)}
-            />
+            <div
+              css={css`
+                position: relative;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+              `}
+            >
+              <Card key={index} src={post.imgUrl} />
+              <CardHoverStyle onClick={() => navigate(`/detail/${post.postId}`)}>
+                <Col justifyContent="center" alignItems="center">
+                  <Row justifyContent="center" alignItems="center" gap={4}>
+                    <div
+                      css={css`
+                        font-size: 3.2rem;
+                        font-weight: 600;
+                        line-height: 42px;
+                        font-family: 'Hancom Sans';
+                        color: ${colors.white};
+                      `}
+                    >
+                      In&Out
+                    </div>
+                    <EyesIcon />
+                  </Row>
+                  <Txt variant="t16" color={colors.lightGray}>
+                    투표하러가기
+                  </Txt>
+                </Col>
+              </CardHoverStyle>
+            </div>
           ))}
         </CardContainer>
       )}
@@ -48,9 +75,37 @@ const CardContainer = styled.div`
 `;
 
 const Card = styled.img`
+  position: relative;
   width: 100%;
   object-fit: cover;
   aspect-ratio: 1 / 1;
   background-color: lightgray;
   border-radius: 2px;
+`;
+
+const CardHoverStyle = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  top: 0;
+  left: 0;
+  right: 0;
+  width: 100%;
+  aspect-ratio: 1 / 1;
+  background-color: transparent;
+  border-radius: 2px;
+  border: transparent;
+  cursor: pointer;
+  opacity: 0;
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.7);
+    border: 2px solid ${colors.yellow900};
+    opacity: 1;
+  }
+  &:active {
+    background-color: rgba(0, 0, 0, 0.7);
+    border: 2px solid ${colors.yellow900};
+    opacity: 1;
+  }
 `;
