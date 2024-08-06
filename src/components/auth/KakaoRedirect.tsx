af4cabe51e0312a740e2ffc75d253ad6239e3def
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-08-06 20:49:32 +0900

    feat: kakao 설정

--- src/components/auth/KakaoRedirect.tsx
+++ src/components/auth/KakaoRedirect.tsx
@@ -2,10 +2,19 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useMutation } from 'react-query';
 import axios from 'axios';
+import useAuthStore from '@stores/auth';
+
+interface KakaoLoginResponse {
+  accessToken: string;
+  memberId: number;
+  memberImageId: number;
+  member: boolean;
+}
 
 const KakaoRedirect = () => {
   const [code, setCode] = useState<string | null>(null);
   const navigate = useNavigate();
+  const { storeProfile } = useAuthStore();
 
   useEffect(() => {
     const urlCode = new URL(window.location.href).searchParams.get('code');
@@ -17,7 +26,8 @@ const KakaoRedirect = () => {
       const response = await axios.get(`https://api.stuffinout.site/kakaologin?code=${code}`, {
         headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' },
       });
-      return response;
+      console.log('Response received:', response.data);
+      return response.data;
     } catch (error) {
       throw error;
     }
@@ -29,8 +39,15 @@ const KakaoRedirect = () => {
     isError,
     data,
   } = useMutation(kakaoLogin, {
-    onSuccess: (data) => {
+    onSuccess: (data: KakaoLoginResponse) => {
       console.log('Login successful:', data);
+      if (data.member) {
+        storeProfile(data.memberId, data.memberImageId, data.accessToken);
+        navigate('/');
+      } else {
+        storeProfile(data.memberId, data.memberImageId, data.accessToken);
+        navigate('/setting');
+      }
     },
     onError: (error) => {
       console.error('Login failed:', error);
